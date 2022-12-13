07280e8a45b212443cf5f1b19fcf30d165348032
soojiin <skim69068@gmail.com>
2022-12-13 16:26:17 +0900

    docs: update calendar

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -3,12 +3,18 @@ import styled from "styled-components";
 const H1 = styled.h1`
     color:white;
     margin-top: 2rem;
+    margin-bottom: 2rem;
+`;
+
+const H3 = styled.h3`
+    color: white;
 `;
 
 const Calendar = () => {
     return (
         <>
             <H1>Calendar</H1>
+            <H3>서비스 준비중...</H3>
             
         </>
     )
